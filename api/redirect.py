fdb776fc6317c0063fdf8c62cd0e62dc8a3e4b27
vedas-dixit <vedasdixit1006@gmail.com>
2025-06-22 17:20:59 +0530

    fixed static link

--- api/redirect.py
+++ api/redirect.py
@@ -81,7 +81,7 @@ async def handle_redirect(
     # Protected
     if url.is_protected:
         return RedirectResponse(
-            url=f"http://localhost:3000/secure/{short_code}", status_code=307
+            url=f"{WEB_BASE_URL}/{short_code}", status_code=307
         )
 
     background_tasks.add_task(record_click, url.id, request)
